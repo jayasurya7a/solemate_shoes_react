fe3995b6d0c0cd08be65353aa2f08e1dc52740ee
Jayasurya.K <jayasurya7aa@gmail.com>
2023-04-21 23:46:51 +0530

    added home.js

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+      This is the home page
+    </div>
+  );
+};
+
+export default Home;
