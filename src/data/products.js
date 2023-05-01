43357b465f1c2bc714fe8c856e99cc3166f811ef
Jayasurya.K <jayasurya7aa@gmail.com>
2023-05-01 13:33:53 +0530

    added product page

---
+++ src/data/products.js
@@ -0,0 +1,67 @@
+const products = [
+    {
+      id: 1,
+      name: "Nike Air Max 270 React",
+      price: 150,
+      image: "https://via.placeholder.com/300x300.png?text=Nike+Air+Max+270+React",
+      brand: "Nike",
+      color: "black",
+      size: "10",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 2,
+      name: "Adidas Ultraboost 21",
+      price: 180,
+      image: "https://via.placeholder.com/300x300.png?text=Adidas+Ultraboost+21",
+      brand: "Adidas",
+      color: "white",
+      size: "9",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+      {
+        id: 3,
+        name: "New Balance 990v5",
+        price: 175,
+        image: "https://via.placeholder.com/300x300.png?text=New+Balance+990v5",
+        brand: "New Balance",
+        color: "gray",
+        size: "US 11",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      },
+      {
+        id: 4,
+        name: "Reebok Nano 9",
+        price: 130,
+        image: "https://via.placeholder.com/300x300.png?text=Reebok+Nano+9",
+        brand: "Reebok",
+        color: "blue",
+        size: "US 10.5",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      },
+      {
+        id: 5,
+        name: "Puma Future Rider",
+        price: 100,
+        image: "https://via.placeholder.com/300x300.png?text=Puma+Future+Rider",
+        brand: "Puma",
+        color: "red",
+        size: "US 9",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      },
+      {
+        id: 6,
+        name: "HOVR Phantom",
+        price: 140,
+        image: "https://via.placeholder.com/300x300.png?text=Under+Armour+HOVR+Phantom",
+        brand: "Under Armour",
+        color: "black",
+        size: "US 11.5",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      }
+      
+    // Add more products as needed
+  ];
+  
+  export default products;
+  
